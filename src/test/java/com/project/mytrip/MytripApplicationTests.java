af1193d05945522e10ac4560f63ea2f071661eb7
suzzung <kidsj0314@naver.com>
2024-11-23 18:02:05 +0900

    Initialize

---
+++ src/test/java/com/project/mytrip/MytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
